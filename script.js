3099aa5ba8a57df67d50580dfd0a0250cdf4abe1
jjporter <jjporter921@gmail.com>
2019-09-23 09:25:58 -0400

    got name field to work

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log('Add validation!');
+function query (selector) {
+    return document.querySelector(selector)
+}
+
+function markValid(field) {
+    field.parentNode.classList.add()
+}
+
+function markInvalid(){
+
+}
+
+function validateName() {
+    let nameField = query('#name')
+    if(nameField.value === "") {
+        nameField.parentNode.classList.remove('input-valid')
+        nameField.parentNode.classList.add('input-invalid')
+    } else{
+        nameField.parentNode.classList.remove('input-invalid')
+        nameField.parentNode.classList.add('input-valid')
+    } 
+}  
+
+// function validateDate(date) {
+//     let today = new date(date.UTC(0,0,0,0))
+//     if(date === "") {
+//         return null
+//     }
+//     if (date === today) {
+//         return true
+//     }
+// }
+
+function numberOfDays() {
+    let daysField = query('#days')
+    if(daysField === "") {
+        return null
+    }
+}
+
+// function markValid(field) {
+//     nameField.parentNode.classList.remove('input-invalid')
+//     nameField.parentNode.classList.add('input-valid')
+// }
+
+// function markValid(field) {
+//     nameField.parentNode.classList.remove('input-vvalid')
+//     nameField.parentNode.classList.add('input-invalid')
+
+
+query('#parking-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    validateName() 
+    numberOfDays()
+
+})
